ec07109fd6c72fe27fc63b71cfbc3fedcf7738b2
Hiep Nguyen Si <hiepnguyensi@HiepnoMacBook-Pro.local>
2023-06-19 21:55:15 +0700

    Init project

---
+++ src/app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const compression = require('compression')
+
+const app = express()
+
+//init middle wares
+// đầu ra ngắn gọn: status có màu/  thời gian phản hồi => chỉ dùng lúc dev
+app.use(morgan('dev'))
+
+// Đầu ra full thông tin: request's ip/ time/ phương thức/ status/ kiểu request. 
+// app.use(morgan('combined'))
+
+// cũng như combined nhưng k biết dùng gì để gửi request
+// app.use(morgan('common'))
+
+// ngắn hơn
+// app.use(morgan('short'))
+
+// ngắn nhất.
+// app.use(morgan('tiny'))
+
+app.use(helmet())
+app.use(compression())
+
+//init db
+
+// init routes
+app.get('/', (req, res, next) => {
+  return res.status(200).json({
+    message: 'Wellcome to eCommerce'
+  })
+})
+
+
+// handling errors
+
+
+module.exports = app
